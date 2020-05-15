fc52afb5a6af5281c986300f9cee8f373f554fc3
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2020-05-15 20:30:22 +0100

    UI improvements

--- app/src/main/java/uk/marcin/scrumpoker/CardDetailsFragment.kt
+++ app/src/main/java/uk/marcin/scrumpoker/CardDetailsFragment.kt
@@ -1,11 +1,16 @@
 package uk.marcin.scrumpoker
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.airbnb.mvrx.*
 import kotlinx.android.synthetic.main.fragment_card_details.*
+
 
 data class CardDetailsState(val isShowingCard: Boolean = false, val selectedCard: String = "") : MvRxState {
     constructor(arg: String): this(selectedCard = arg)
@@ -23,11 +28,49 @@ class CardDetailsViewModel(initialState: CardDetailsState) : MvRxViewModel<CardD
 class CardDetailsFragment : BaseMvRxFragment() {
     private val viewModel: CardDetailsViewModel by fragmentViewModel()
     private val selectedCard: String by args()
+    private var isInitalised: Boolean = false
 
-    override fun invalidate() = withState(viewModel) {
-        textView_selectedCard.text = if (it.isShowingCard) it.selectedCard else "X"
-        textView_selectedCardInverted.text = if (it.isShowingCard) it.selectedCard else "X"
-        textView_hint.text = if (it.isShowingCard) "Tap to hide your card" else "Tap to show your card"
+    override fun invalidate() = withState(viewModel) { state ->
+        textView_hint.text = if (state.isShowingCard) "Tap to hide your card" else "Tap to show your card"
+
+        if (!isInitalised){
+            isInitalised = true
+            return@withState
+        }
+
+        cardView_cardContainer.animate().withLayer()
+            .rotationY(90f)
+            .rotationX(-20F)
+            .setDuration(80)
+            .withEndAction {
+                val v = cardView_cardContainer
+
+                val scale: Float =
+                    this.requireActivity().getApplicationContext()
+                        .getResources().getDisplayMetrics().density
+                val distance: Float =
+                    v.getCameraDistance() * (scale + scale / 20)
+                if (distance.isFinite() && distance > 0)
+                    v.cameraDistance = distance
+
+                val colorId = if (state.isShowingCard) R.color.colorAccent else R.color.colorPrimaryLight
+                val color = ContextCompat.getColor(this.requireContext(), colorId);
+//                if (it.isShowingCard)
+//                    cardView_cardContainer.setCardBackgroundColor(color)
+                textView_cardSelected.isVisible = state.isShowingCard == false
+                textView_selectedCard.text = if (state.isShowingCard) state.selectedCard else ""
+                textView_selectedCardInverted.text = if (state.isShowingCard) state.selectedCard else ""
+
+                v.rotationY = -90f;
+                v.rotationX = -20f;
+                v.animate().withLayer()
+                    .rotationY(0f)
+                    .rotationX(0F)
+                    .setDuration(150)
+                    .withEndAction {
+                    }
+                    .start();
+            }.start();
     }
 
     override fun onCreateView(
